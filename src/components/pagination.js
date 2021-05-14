72a6cfea2355b2b3c5c88b14af2d500a5b4f3534
roganoalien <hola@rodrigogarcia.com.mx>
2021-05-14 06:41:12 -0500

    :lock: Warnings fixed
    Fixed some warnings about declarations not used

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 // local
